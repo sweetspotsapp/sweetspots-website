9be174838bbb2eeab1ea93ee369aed0d05c5cb86
FarizioKautsar <kheruzy@gmail.com>
2025-09-25 08:54:29 +1000

    Zod email

--- components/early-user/EarlyUserForm.tsx
+++ components/early-user/EarlyUserForm.tsx
@@ -29,7 +29,7 @@ import { createEarlyUser } from "@/api/early-users/endpoints";
 
 const schema = z.object({
   name: z.string().min(1, "Name is required"),
-  email: z.email("Please enter a valid email"),
+  email: z.string().email("Please enter a valid email"),
   // appVersion: z.enum(["alpha", "beta", "public"]),
 });
 type Values = z.infer<typeof schema>;
